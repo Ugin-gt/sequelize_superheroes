d766bcaf71713324e4818ead9b47c165dc1beb0a
Ugin-gt <apach575@gmail.com>
2021-04-07 17:07:47 +0300

    Add app.js & http

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const router = require('./routes');
+const router = require('./routes/superhero');
 const errorHandler = require('./middlewares/error.handlers');
-const { STATIC_PATH } = require('./config/config');
 const app = express();
 
-app.use(express.static(STATIC_PATH));
+app.use(express.static('public'));
 
 app.use(express.json());
 
