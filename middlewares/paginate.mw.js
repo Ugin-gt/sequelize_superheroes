9448742495db1cf7a49a597136e26191a659027c
Ugin-gt <apach575@gmail.com>
2021-04-08 17:06:29 +0300

    Add pagination & multer image

--- middlewares/paginate.mw.js
+++ middlewares/paginate.mw.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
       query: { limit, offset },
     } = req;
     req.pagination = {
-      limit: limit > 5 || limit <= 0 ? 5 : limit,
+      limit: limit > 5 || limit <= 0 ? limit : 5,
       offset: offset <= 0 ? 0 : offset,
     };
     next();
--- routes/superhero.js
+++ routes/superhero.js
@@ -4,6 +4,15 @@ const multer = require('multer');
 const HeroController = require('../controller/superhero.controller');
 const paginate = require('../middlewares/paginate.mw');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, STATIC_PATH);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}.${file.originalname}`);
+  },
+});
+
 const upload = multer({ dest: path.resolve(__dirname, '../public/images') });
 const heroRouter = Router();
 
